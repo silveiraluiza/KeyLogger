124fec1d25a47d13d9ff7753f6d9f99c736323b8
Luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2017-12-12 11:48:24 -0300

    created post of get all

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var Registro = require('../models/registro');
 var hoje = new Date();
 
-// Get Homepage
+// Get Homepage (last 5)
 router.get('/', ensureAuthenticated, function(req, res){
 
 	Registro.getRegistrobyDia(hoje.getDate() + '/' + (hoje.getMonth()+1) + '/' + hoje.getFullYear(), function(err, registro){
@@ -27,12 +27,34 @@ router.get('/', ensureAuthenticated, function(req, res){
 
 });
 
+// get all registros of the day
+router.get('/all', ensureAuthenticated, function(req, res){
+
+	Registro.getAllRegistroOfDia(hoje.getDate() + '/' + (hoje.getMonth()+1) + '/' + hoje.getFullYear(), function(err, registro){
+			if(err) throw err;
+			var ultimos = [];
+			for (i in registro){
+
+				console.log(registro[i]);
+				console.log(registro[i]['nome']);
+
+				ultimos.push(registro[i]['nome'] + 
+					' estava em posse da chave às: ' + registro[i]['hora'] + ' pode ser encontrado(a) no ' +
+					registro[i]['local']);
+
+				console.log(ultimos);
+				
+			}
+			res.render('index', { message: ultimos});
+		});
+
+});
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	} else {
-		//req.flash('error_msg','You are not logged in');
+		req.flash('error_msg','You are not logged in');
 		res.redirect('/users/login');
 	}
 }
